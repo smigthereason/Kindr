c065ea46fc3753c21e3af828dabef139df223f48
Victor <victor.dmaina@gmail.com>
2024-08-03 16:23:24 +0300

    install tailwind and add Landing page template

---
+++ frontend/src/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import { FaHandsHelping } from "react-icons/fa";
+
+const LandingPage: React.FC = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default LandingPage;
